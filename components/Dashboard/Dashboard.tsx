a1f8a7a4a895963eeca70bbfcc469054be86a6f6
Dimatimmo <dimatimmokh@gmail.com>
2020-02-01 13:56:57 +0200

    add fetching data

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,21 +1,41 @@
 import React, {useEffect } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native';
 
 export default function Dashboard ({navigation}) {
+
+  let [posts, setPosts] = React.useState([]);
   
   function handleSubmit() {
     navigation.navigate('SignIn');
   }
 
-  useEffect( () => {
-    
+  const url = 'https://my-json-server.typicode.com/typicode/demo/posts';
+
+  useEffect(() => {
+    getData();
   })
 
+  async function getData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    setPosts(posts = data);
+  }
   
+  const renderItem = ({item}) => {
+    return(
+    <View style={styles.postsList}>
+      <Text style = {styles.postHead}>PostId: {item.id}</Text>
+      <Text style = {styles.postsContent}>Post Title: {item.title}</Text>
+    </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.textMessage}>Hello</Text>
+      <FlatList 
+        data={posts}
+        renderItem={renderItem}
+      />
       <Button onPress={handleSubmit} title="Logout"></Button>
     </View>
     );
@@ -23,7 +43,7 @@ export default function Dashboard ({navigation}) {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 50,
+    paddingTop: 10,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -34,5 +54,24 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start',
     marginBottom: 10
     
+  },
+  postsList: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    textAlign: 'center',
+    padding: 10,
+
+  },
+  postHead: {
+    textAlign: 'center',
+    alignSelf: 'stretch',
+    fontWeight: 'bold'
+  },
+  postsContent: {
+    textAlign: 'center',
+    alignSelf: 'stretch',
   },
 });
